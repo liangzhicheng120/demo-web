74bdd29bb0ab6b7421659cb5684fbb0877f15a5c
liangzhicheng <liangzhicheng@meizu.com>
2017-03-15 18:21:26 +0800

    #20173151821 增加分词接口
    Change-Id: Ic7978d666fe12c045879578dbccdad083aacb0bf
    Signed-off-by: liangzhicheng <liangzhicheng@meizu.com>

---
+++ src/main/java/com/xinrui/demo/action/HanLPConstroller.java
@@ -0,0 +1,33 @@
+package com.xinrui.demo.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.fastjson.JSONObject;
+import com.hankcs.hanlp.HanLP;
+import com.xinrui.demo.bean.BaseResultModel;
+
+@Controller
+@RequestMapping(value = "/hanlp")
+public class HanLPConstroller {
+
+	@RequestMapping(value = "/extractKeyword")
+	@ResponseBody
+	public BaseResultModel extractKeyword(String document, int size) {
+		BaseResultModel baseResultModel = new BaseResultModel();
+		List<String> keys = HanLP.extractKeyword(document, size);
+		List<JSONObject> value = new ArrayList<JSONObject>();
+		for (String key : keys) {
+			JSONObject e = new JSONObject();
+			e.put("key", key);
+			value.add(e);
+		}
+		baseResultModel.setValue(value);
+		return baseResultModel;
+	}
+
+}
