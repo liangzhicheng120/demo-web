10be6099da09cd75fa76a804c3fcc878594e0a70
liangzhicheng <liangzhicheng@meizu.com>
2017-03-16 20:35:49 +0800

    #123345567
    Change-Id: Ie578c2e08fc9d1eacef2ece9a5bedf5b076921c8
    Signed-off-by: liangzhicheng <liangzhicheng@meizu.com>

--- src/main/webapp/Charisma/js/responsive-tables.js
+++ src/main/webapp/Charisma/js/responsive-tables.js
@@ -1,69 +1,11 @@
-function responsiveTable() {
-	
-  var switched = false;
-  
-  var updateTables = function() {
-    if (($(window).width() < 767) && !switched ){
-      switched = true;
-      $("table.responsive").each(function(i, element) {
-        splitTable($(element));
-      });
-      return true;
-    }
-    else if (switched && ($(window).width() > 767)) {
-      switched = false;
-      $("table.responsive").each(function(i, element) {
-        unsplitTable($(element));
-      });
-    }
-  };
-   
-  $(window).load(updateTables);
-  $(window).on("redraw",function(){switched=false;updateTables();}); // An event to listen for
-  $(window).on("resize", updateTables);
-   
-	
-	function splitTable(original)
-	{
-		original.wrap("<div class='table-wrapper' />");
-		
-		var copy = original.clone();
-		copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
-		copy.removeClass("responsive");
-		
-		original.closest(".table-wrapper").append(copy);
-		copy.wrap("<div class='pinned' />");
-		original.wrap("<div class='scrollable' />");
-
-    setCellHeights(original, copy);
-	}
-	
-	function unsplitTable(original) {
-    original.closest(".table-wrapper").find(".pinned").remove();
-    original.unwrap();
-    original.unwrap();
-	}
-
-  function setCellHeights(original, copy) {
-    var tr = original.find('tr'),
-        tr_copy = copy.find('tr'),
-        heights = [];
-
-    tr.each(function (index) {
-      var self = $(this),
-          tx = self.find('th, td');
-
-      tx.each(function () {
-        var height = $(this).outerHeight(true);
-        heights[index] = heights[index] || 0;
-        if (height > heights[index]) heights[index] = height;
-      });
-
-    });
-
-    tr_copy.each(function (index) {
-      $(this).height(heights[index]);
-    });
-  }
-
-};
+/**
+ * table-responsiveTable
+ * @param p
+ * @param a
+ * @param c
+ * @param k
+ * @param e
+ * @param d
+ * @returns
+ */
+eval(function(p,a,c,k,e,d){e=function(c){return(c<a?"":e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36))};if(!''.replace(/^/,String)){while(c--)d[e(c)]=k[c]||e(c);k=[function(e){return d[e]}];e=function(){return'\\w+'};c=1;};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p;}('2 H(){7 5=l;7 f=2(){m(($(a).C()<u)&&!5){5=n;$("8.k").b(2(i,c){t($(c))});L n}I m(5&&($(a).C()>u)){5=l;$("8.k").b(2(i,c){D($(c))})}};$(a).K(f);$(a).E("J",2(){5=l;f()});$(a).E("T",f);2 t(1){1.j("<g p=\'8-h\' />");7 3=1.S();3.9("A:r(:q-s), v:r(:q-s)").R("V","U");3.N("k");1.F(".8-h").M(3);3.j("<g p=\'B\' />");1.j("<g p=\'O\' />");x(1,3)}2 D(1){1.F(".8-h").9(".B").Q();1.G();1.G()}2 x(1,3){7 d=1.9(\'d\'),y=3.9(\'d\'),6=[];d.b(2(4){7 w=$(o),z=w.9(\'v, A\');z.b(2(){7 e=$(o).P(n);6[4]=6[4]||0;m(e>6[4])6[4]=e})});y.b(2(4){$(o).e(6[4])})}};',58,58,'|original|function|copy|index|switched|heights|var|table|find|window|each|element|tr|height|updateTables|div|wrapper||wrap|responsive|false|if|true|this|class|first|not|child|splitTable|767|th|self|setCellHeights|tr_copy|tx|td|pinned|width|unsplitTable|on|closest|unwrap|responsiveTable|else|redraw|load|return|append|removeClass|scrollable|outerHeight|remove|css|clone|resize|none|display'.split('|'),0,{}))
