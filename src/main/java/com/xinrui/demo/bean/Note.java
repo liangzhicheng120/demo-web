4cc16401ce8a64290ed3f991cf397647f4d47be9
liangzhicheng <liangzhicheng@meizu.com>
2017-03-16 09:40:37 +0800

    #2017316940 添加主体内容
    Change-Id: Ic1d7ea1b472d85d13d219dc86aa2c44882116796
    Signed-off-by: liangzhicheng <liangzhicheng@meizu.com>

---
+++ src/main/java/com/xinrui/demo/bean/Note.java
@@ -0,0 +1,57 @@
+package com.xinrui.demo.bean;
+
+import java.sql.Date;
+
+public class Note {
+	
+	private int id;
+	
+	private String Content;
+	
+	private String keyword;
+	
+	private Date posttime;
+	
+	private int aid;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return Content;
+	}
+
+	public void setContent(String content) {
+		Content = content;
+	}
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public Date getPosttime() {
+		return posttime;
+	}
+
+	public void setPosttime(Date posttime) {
+		this.posttime = posttime;
+	}
+
+	public int getAid() {
+		return aid;
+	}
+
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+
+}
---
+++ src/test/resource/dao/NoteDaoTest.java
@@ -0,0 +1,21 @@
+package dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.xinrui.demo.dao.NoteDao;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring/spring-*.xml")
+public class NoteDaoTest {
+	@Autowired
+	private NoteDao NoteDao;
+	
+	@Test
+	public void testGetAll() {
+		NoteDao.getAll();
+	}
+}
