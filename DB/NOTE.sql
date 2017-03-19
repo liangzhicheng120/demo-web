c63630061dba2fd7f9d92da85941e9f06fc6a9f7
kimi <liangzhicheng120@126.com>
2017-03-19 11:39:50 +0800

    #20173191139 添加sql
    Change-Id: I78cc1801aff8a65a652f9c05baecde81872a6b93
    Signed-off-by: kimi <liangzhicheng120@126.com>

--- DB/NOTE.sql
+++ DB/NOTE.sql
@@ -9,9 +9,12 @@ ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COMMENT = '笔记内容';
 
+ALTER TABLE `demo_web`.`note` 
+CHANGE COLUMN `id` `id` INT(11) NOT NULL AUTO_INCREMENT ;
+
 
 SELECT * FROM demo_web.note;
-INSERT INTO `demo_web`.`note` ( `content`, `keyword`, `posttime`, `aid`) VALUES ( '这些情况下，动态请求返回的数据一般不是已拼好的 HTML 就是 JSON 或 XML，总之不在浏览器端拼数据就在服务器端拼数据。不过，从传输量方面来看，返回 HTML 不划算，而在 web 传输方面，现在更多的是使用 JSON 而不是 XML。', '详解', '2017-03-15 20:14:17', '2');
+INSERT INTO `demo_web`.`note` ( `content`, `keyword`, `posttime`, `aid` ,`title`) VALUES ( '这些情况下，动态请求返回的数据一般不是已拼好的 HTML 就是 JSON 或 XML，总之不在浏览器端拼数据就在服务器端拼数据。不过，从传输量方面来看，返回 HTML 不划算，而在 web 传输方面，现在更多的是使用 JSON 而不是 XML。', '详解', '2017-03-15 20:14:17', '2','json');
 
 ALTER TABLE `demo_web`.`note` 
 ADD COLUMN `title` VARCHAR(100) CHARACTER SET 'utf8' NOT NULL COMMENT '标题' AFTER `aid`;
